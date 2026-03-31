c97b517b1af91e4e81dbf86377470b7f47607137
sakshi <sakshi08062004@gmail.com>
2026-03-31 08:53:27 +0530

    Initial commit: Python Flask To-Do List app with add/update/delete tasks

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# To-Do List App GitHub Setup  
+- [ ] Step 1: Create .gitignore  
+- [ ] Step 2: git init  
+- [ ] Step 3: git add .  
+- [ ] Step 4: git commit -m "Initial To-Do List app"  
+- [ ] Step 5: gh repo create 202504104610132_A --public --push  
+- [ ] Step 6: Test app locally
---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form.get('task')
+    tasks.append({'task': task, 'done': False})
+    return redirect('/')
+
+@app.route('/update/<int:index>')
+def update(index):
+    tasks[index]['done'] = not tasks[index]['done']
+    return redirect('/')
+
+@app.route('/delete/<int:index>')
+def delete(index):
+    tasks.pop(index)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
